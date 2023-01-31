edc90caa0d85f093181d149807a257ec0409bf35
Olushola <olushola251@gmail.com>
2023-01-31 19:40:46 +0100

    working on the header

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,tsx,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        headerBorderColor: "#373B40"
+      }
+    },
+  },
+  plugins: [],
+}
